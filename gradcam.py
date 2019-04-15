4c31e8621122eded745b52f95536ba9e0868ed88
Sooram <rkdtnfka@naver.com>
2019-04-15 10:06:06 +0900

    Update gradcam.py

--- gradcam.py
+++ gradcam.py
@@ -11,6 +11,11 @@ Reference: https://github.com/Ankush96/grad-cam.tensorflow/blob/master/main.py
 import numpy as np
 import tensorflow as tf
 
+import PIL 
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import vgg16
 
 config = {
     "img_W": 224,
@@ -43,60 +48,63 @@ def get_classmap(target_class, last_layer, conv, cmap_width, cmap_height):
     
     return classmap
  
-# %%
-import numpy as np
-import tensorflow as tf
+# %% util functions
+def load_img(filename):
+    img = Image.open(filename)
 
-from PIL import Image
-import PIL 
-import matplotlib.pyplot as plt
+    # preprocess the img
+    img0 = img.resize((config["img_W"], config["img_H"]), resample=PIL.Image.NEAREST)
+    img0 = np.array(img0)
+    img0 = img0 / 255.0
+    img_reshaped = np.reshape(img0, [-1, config["img_W"], config["img_H"], 3])
+    
+    return img, img_reshaped
+    
+def open_sess(gpu_num):
+    c = tf.ConfigProto()
+    c.gpu_options.visible_device_list = gpu_num    #"0" 
+    
+    sess = tf.Session(config=c)
+    sess.run(tf.global_variables_initializer())
+    
+    return sess
 
-import vgg16
+# %% main
+def main():
+    # load an img file, preprocess and reshape it
+    img_original, img_reshaped = load_img('./Black_Footed_Albatross_0001_796111.jpg')
+
+    # open a session
+    sess = open_sess("0")
+
+    # build a model
+    images = tf.placeholder("float", [1, config["img_W"], config["img_H"], 3])
+    
+    vgg = vgg16.Vgg16()
+    vgg.build(images)
+    
+    feed_dict = {images: img_reshaped}
+    prob = sess.run(vgg.prob, feed_dict=feed_dict)
+    #print(prob)
+
+    # get the class map value
+    target_class = [np.argmax(prob)]    # the class that we're interested in        
+    output = vgg.fc8                    # the output(score) layer before the softmax 
+    last_conv = vgg.conv5_3             # the last feature maps
+    
+    label = tf.placeholder(tf.int64, [None], name='label')
+    classmap = get_classmap(label, output, last_conv, config["img_W"], config["img_H"])
+    
+    gradcam = sess.run(classmap, feed_dict={images: img_reshaped,
+                                            label: target_class})
+    
+    # draw the class map
+    plt.imshow(img_original.resize([config["img_W"], config["img_H"]]), cmap = 'gray' )
+    #plt.imshow(gradcam[0], cmap=plt.cm.seismic, alpha=0.5, interpolation='nearest')    # normalized
+    plt.imshow(gradcam[0], cmap=plt.cm.seismic, alpha=0.5, interpolation='nearest', vmin=-0.4, vmax=0.4)
+    plt.savefig('test.png', dpi=256)
+    plt.close() 
 
 
-""" load & preprocess imgs """
-img = Image.open('./Black_Footed_Albatross_0001_796111.jpg')
-
-# preprocess the img
-img0 = img.resize((config["img_W"], config["img_H"]), resample=PIL.Image.NEAREST)
-img0 = np.array(img0)
-img0 = img0 / 255.0
-img_reshaped = np.reshape(img0, [-1, config["img_W"], config["img_H"], 3])
-
-""" open a session """
-gpu_num = "0"
-
-c = tf.ConfigProto()
-c.gpu_options.visible_device_list = gpu_num    #"0" 
-
-sess = tf.Session(config=c)
-sess.run(tf.global_variables_initializer())
-
-""" build a model """
-images = tf.placeholder("float", [1, config["img_W"], config["img_H"], 3])
-
-vgg = vgg16.Vgg16()
-vgg.build(images)
-
-feed_dict = {images: img_reshaped}
-prob = sess.run(vgg.prob, feed_dict=feed_dict)
-#print(prob)
-
-
-""" draw a class map """
-target_class = [np.argmax(prob)]    # the class that we're interested in        
-output = vgg.fc8    # the layer with output scores before the softmax layer 
-last_conv = vgg.conv5_3     # the last feature maps
-
-label = tf.placeholder(tf.int64, [None], name='label')
-classmap = get_classmap(label, output, last_conv, config["img_W"], config["img_H"])
-
-gradcam = sess.run(classmap, feed_dict={images: img_reshaped,
-                                        label: target_class})
-
-# show the cam
-plt.imshow(img.resize([config["img_W"], config["img_H"]]), cmap = 'gray' )
-#plt.imshow(gradcam[0], cmap=plt.cm.seismic, alpha=0.5, interpolation='nearest')    # normalized
-plt.imshow(gradcam[0], cmap=plt.cm.seismic, alpha=0.5, interpolation='nearest', vmin=-0.4, vmax=0.4)
-plt.savefig('test.png', dpi=256)
-plt.close() 
+if __name__ == '__main__':
+    main()
